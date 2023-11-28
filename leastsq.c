c304fbdbe7e45c93c755835f33254bee06253d62
Kwashie A <104215256+Kwash67@users.noreply.github.com>
2023-11-28 22:58:37 +0000

    improving robustness

--- leastsq.c
+++ leastsq.c
@@ -1,65 +1,75 @@
 /* My Reg Number: 230184136 */
 #include <stdio.h>
 
-// GLOBAL VARIABLES
-int try_again = 0;
-float sum_of_pdt = 0;
-float sum_of_x = 0;
-float sum_of_y = 0;
-float sum_of_x_squared = 0;
+/* GLOBAL CONSTANTS */
+const int min_coords=4, max_coords=20; 
+const float min_value=0.000005, max_value=1000000.0;
 
-// FUNCTIONS USED
-float accept_value(char item);
-void calc_sums(float coord_list[][2], int rows);
-void calc_gradient(int rows);
-void calc_offset(int rows);
-void retry_request();
+/* FUNCTION PROTOTYPES */
+float validate_input(char item);
+void display_table(float coord_list[][2], int rows);
+float calc_sum_of_x(float coord_list[][2], int rows);
+float calc_sum_of_y(float coord_list[][2], int rows);
+float calc_sum_of_pdt(float coord_list[][2], int rows);
+float calc_sum_of_x_squared(float coord_list[][2], int rows);
+void calc_gradient(int rows, float sum_of_x, float sum_of_y, float sum_of_pdt, float sum_of_x_squared);
+void calc_offset(int rows, float sum_of_x, float sum_of_y, float sum_of_pdt, float sum_of_x_squared);
+int retry_request();
 
 int main(void)
 {
+    int try_again = 0;
     do
     {
+        /* Reset all Sums to Zero on every try */
+        float sum_of_x = 0;
+        float sum_of_y = 0;
+        float sum_of_pdt = 0; 
+        float sum_of_x_squared = 0;
+
         printf("This program calculates gradient and offset.\n");
         int rows;
         printf("Enter the number of coordinate pairs (between 4 to 20): ");
-        rows = (int)accept_value('r'); // Refer to line 63 for details of function
+        rows = (int)validate_input('r'); 
         float coord[rows][2];
         printf("Enter the data coordinates in the table below.\n");
         printf("\n");
-        int a, b;
-        for (b = 0; b < 2; b++)
+        int row, col;
+        for (col = 0; col < 2; col++)
         {
-            if (b == 0)
-            {
+            if (col == 0)
                 printf("X values\n--------\n");
-            }
-            if (b == 1)
-            {
+            else if (col == 1)
                 printf("Y values\n--------\n");
-            }
-            for (a = 0; a < rows; a++)
+            for (row = 0; row < rows; row++)
             {
                 printf("   ");
-                coord[a][b] = accept_value('p'); // Refer to line 63 for details of function
+                coord[row][col] = validate_input('p'); 
             }
         }
-        printf("\n"); // Displaying the collected values
-        printf("X values \t|\tY values\n--------------------------------\n");
-        for (a = 0; a < rows; a++)
-        {
-            printf("%f\t|\t%f", coord[a][0], coord[a][1]);
-            printf("\n");
-        }
-        calc_sums(coord, rows);
-        calc_gradient(rows);
-        calc_offset(rows);
-        retry_request();
+        
+        /* Displaying collected values */
+        display_table(coord, rows);
+
+        /* Calculating Sums */
+        sum_of_x= calc_sum_of_x(coord, rows);
+        sum_of_y= calc_sum_of_y(coord, rows);
+        sum_of_pdt= calc_sum_of_pdt(coord, rows);
+        sum_of_x_squared= calc_sum_of_x_squared(coord, rows);
+
+        /* Calculating and displaying gradient and offset */
+        calc_gradient(rows, sum_of_x, sum_of_y, sum_of_pdt, sum_of_x_squared);
+        calc_offset(rows, sum_of_x, sum_of_y, sum_of_pdt, sum_of_x_squared);
+
+        /* Request to try again */
+        try_again = retry_request();
+
     } while (try_again == 1);
+    return(0);
 }
 
 // FUNCTION DEFINITIONS
-
-float accept_value(char item)
+float validate_input(char item)
 {
     /* item = 'r' for coord pairs, item = 'p' for x or y value */
     if (item == 'r')
@@ -69,11 +79,11 @@ float accept_value(char item)
         {
             scanf("%f", &val);
             while(getchar()!='\n');
-            if (val < 4 || val > 20)
+            if (val < min_coords || val > max_coords)
             {
-                printf("The number must be between 4 and 20 (inclusive)\n   ");
+                printf("The number must be between %d and %d (inclusive): ", min_coords, max_coords);
             }
-        } while (val < 4 || val > 20);
+        } while (val < min_coords || val > max_coords);
         return val;
     }
     else
@@ -83,38 +93,77 @@ float accept_value(char item)
         {
             scanf("%f", &val2);
             while(getchar()!='\n');
-            if (val2 < 0.000005 || val2 > 1000000.0)
+            if (val2 < min_value || val2 > max_value)
             {
-                printf("The number must be between 0.000005 and 1000000.0\n   ");
+                printf("The number must be between %f and %f\n   ", min_value, max_value);
             }
-        } while (val2 < 0.000005 || val2 > 1000000.0);
+        } while (val2 < min_value || val2 > max_value);
         return val2;
     }
 }
-void calc_sums(float coord_list[][2], int rows)
+void display_table(float coord_list[][2], int rows){
+    int index;
+    printf("\n");
+        printf("X values \t|\tY values\n--------------------------------\n");
+        for (index = 0; index < rows; index++)
+        {
+            printf("%f\t|\t%f", coord_list[index][0], coord_list[index][1]);
+            printf("\n");
+        }
+}
+float calc_sum_of_x(float coord_list[][2], int rows)
 {
-    int a;
-    for (a = 0; a < rows; a++)
+    int index;
+    float sum_of_x=0;
+    for (index = 0; index < rows; index++)
     {
-        sum_of_x += coord_list[a][0];
-        sum_of_y += coord_list[a][1];
-        sum_of_pdt += (coord_list[a][0]) * (coord_list[a][1]);
-        sum_of_x_squared += (coord_list[a][0]) * (coord_list[a][0]);
+        sum_of_x += coord_list[index][0];
     }
+    return sum_of_x;
 }
-void calc_gradient(int rows)
+float calc_sum_of_y(float coord_list[][2], int rows)
 {
-    float m;
-    m = ((rows * sum_of_pdt) - (sum_of_x * sum_of_y)) / ((rows * sum_of_x_squared) - (sum_of_x * sum_of_x));
-    printf("\nm = %f", m);
+    int index;
+    float sum_of_y=0;
+    for (index = 0; index < rows; index++)
+    {
+        sum_of_y += coord_list[index][1];
+    }
+    return sum_of_y;
 }
-void calc_offset(int rows)
+float calc_sum_of_pdt(float coord_list[][2], int rows)
 {
-    float c;
-    c = ((sum_of_x_squared * sum_of_y) - (sum_of_x * sum_of_pdt)) / ((rows * sum_of_x_squared) - (sum_of_x * sum_of_x));
-    printf("\nc = %f", c);
+    int index;
+    float sum_of_pdt =0;
+    for (index = 0; index < rows; index++)
+    {
+        sum_of_pdt += (coord_list[index][0]) * (coord_list[index][1]);
+    }
+    return sum_of_pdt;
 }
-void retry_request()
+float calc_sum_of_x_squared(float coord_list[][2], int rows)
+{
+    int index;
+    float sum_of_x_squared=0;
+    for (index = 0; index < rows; index++)
+    {
+        sum_of_x_squared += (coord_list[index][0]) * (coord_list[index][0]);
+    }
+    return sum_of_x_squared;
+}
+void calc_gradient(int rows, float sum_of_x, float sum_of_y, float sum_of_pdt, float sum_of_x_squared)
+{
+    float gradient;
+    gradient = ((rows * sum_of_pdt) - (sum_of_x * sum_of_y)) / ((rows * sum_of_x_squared) - (sum_of_x * sum_of_x));
+    printf("\nm = %f", gradient);
+}
+void calc_offset(int rows, float sum_of_x, float sum_of_y, float sum_of_pdt, float sum_of_x_squared)
+{
+    float offset;
+    offset = ((sum_of_x_squared * sum_of_y) - (sum_of_x * sum_of_pdt)) / ((rows * sum_of_x_squared) - (sum_of_x * sum_of_x));
+    printf("\nc = %f", offset);
+}
+int retry_request()
 {
     int valid = 0;
     do
@@ -124,19 +173,15 @@ void retry_request()
         scanf(" %c", &response);
         if (response == 'y')
         {
-            sum_of_pdt = 0;
-            sum_of_x = 0;
-            sum_of_y = 0;
-            sum_of_x_squared = 0;
             printf("\n");
-            try_again = 1;
+            return 1;
             valid = 1;
         }
         else if (response == 'n')
         {
             printf("\nGoodbye!\n");
-            try_again = 0;
             valid = 1;
+            return 0;
         }
         else
         {
@@ -144,4 +189,5 @@ void retry_request()
             valid = 0;
         }
     } while (valid == 0);
+    return -1;
 }
