ba4b0112d6683f52433f59fc082346ac8fc67e26
Kwashie A <104215256+Kwash67@users.noreply.github.com>
2023-11-30 13:33:53 +0000

    improving readability through comments

--- leastsq.c
+++ leastsq.c
@@ -6,9 +6,12 @@ const int min_coord_pairs=4, max_coord_pairs=20;
 const float min_value=0.000005, max_value=1000000.0;
 
 /* GLOBAL VARIABLE */
-float coordinates[max_coord_pairs][2];
+float coordinates[max_coord_pairs][2]; // An array big enough to hold all coordinate pairs
 
-/* FUNCTION PROTOTYPES */
+/* 
+   FUNCTION PROTOTYPES 
+   [check below main() to find function definitions]
+*/
 float validate_input(char item);
 void collect_coordinates(int rows);
 void display_table(float coord_list[][2], int rows);
@@ -53,10 +56,19 @@ int main(void)
     return(0);
 }
 
-// FUNCTION DEFINITIONS
+/* FUNCTION DEFINITIONS */ 
 float validate_input(char item)
 {
-    /* item = 'r' for coord pairs (Rows), item = 'v' for x or y Value */
+    /* Purpose: 
+        to validate all scanf inputs to ensure they are within range and of the right type.
+       Inputs: 
+        char item - a character that carries information about the input to be validated.
+        ie: char item= 'r' for coord pairs (Rows)
+            or
+            char item = 'v' for x or y Value 
+       Returned value:
+        float - The validated input.
+    */
     if (item == 'r')
     {
         int coordPairs;
@@ -67,8 +79,7 @@ float validate_input(char item)
 
             if (input_check != 1 || coordPairs < min_coord_pairs || coordPairs > max_coord_pairs) {
                 printf("Invalid input or value out of range. Please enter a valid float between %d and %d (inclusive).\n", min_coord_pairs, max_coord_pairs);
-                // Clear the input buffer
-                while (getchar() != '\n');
+                while (getchar() != '\n'); // Clear the input buffer
                 coordPairs = -1; // Reset value for re-prompting
             }
 
@@ -84,8 +95,7 @@ float validate_input(char item)
 
             if (input_check != 1 || point < min_value || point > max_value) {
                 printf("Invalid input or value out of range. Please enter a valid float between %f and %f.\n   ", min_value, max_value);
-                // Clear the input buffer
-                while (getchar() != '\n');
+                while (getchar() != '\n'); // Clear the input buffer
                 point = -1; // Reset value for re-prompting
             }
 
@@ -95,6 +105,14 @@ float validate_input(char item)
     return 0;
 }
 void collect_coordinates(int rows){
+    /* Purpose: 
+        Collects X values and Y values and stores them in the 2D coordinates array. 
+        It makes use of validate_input('v') to validate each point provided by the user.
+       Inputs: 
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+       Returned value:
+        void
+    */
     printf("\nEnter the data coordinates in the table below.\n");
     printf("\n");
     int row, col;
@@ -112,6 +130,14 @@ void collect_coordinates(int rows){
     }
 }
 void display_table(float coord_list[][2], int rows){
+    /* Purpose: 
+        Neatly displays X and Y values in a table form.
+       Inputs: 
+        float coord_list[][2] - The 2D array containing the coordinate value pairs.
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+       Returned value:
+        void
+    */
     int index;
     printf("\n");
         printf("X values \t|\tY values\n--------------------------------\n");
@@ -123,6 +149,14 @@ void display_table(float coord_list[][2], int rows){
 }
 float calc_sum_of_x(float coord_list[][2], int rows)
 {
+    /* Purpose: 
+        Calculates the sum of all x values.
+       Inputs: 
+        float coord_list[][2] - The 2D array containing the coordinate value pairs.
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+       Returned value:
+        float sum_of_x - The sum of all x values.
+    */
     int index;
     float sum_of_x=0;
     for (index = 0; index < rows; index++)
@@ -133,6 +167,14 @@ float calc_sum_of_x(float coord_list[][2], int rows)
 }
 float calc_sum_of_y(float coord_list[][2], int rows)
 {
+    /* Purpose: 
+        Calculates the sum of all y values.
+       Inputs: 
+        float coord_list[][2] - The 2D array containing the coordinate value pairs.
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+       Returned value:
+        float sum_of_y - The sum of all y values.
+    */
     int index;
     float sum_of_y=0;
     for (index = 0; index < rows; index++)
@@ -143,6 +185,14 @@ float calc_sum_of_y(float coord_list[][2], int rows)
 }
 float calc_sum_of_pdt(float coord_list[][2], int rows)
 {
+    /* Purpose: 
+        Calculates the sum of the product of each x value and its corresponding y value.
+       Inputs: 
+        float coord_list[][2] - The 2D array containing the coordinate value pairs.
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+       Returned value:
+        float sum_of_pdt - The sum of the product of each x value and its corresponding y value.
+    */
     int index;
     float sum_of_pdt =0;
     for (index = 0; index < rows; index++)
@@ -153,6 +203,14 @@ float calc_sum_of_pdt(float coord_list[][2], int rows)
 }
 float calc_sum_of_x_squared(float coord_list[][2], int rows)
 {
+    /* Purpose: 
+        Calculates the sum of each x value squared.
+       Inputs: 
+        float coord_list[][2] - The 2D array containing the coordinate value pairs.
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+       Returned value:
+        float sum_of_x_squared - The sum of each x value squared.
+    */
     int index;
     float sum_of_x_squared=0;
     for (index = 0; index < rows; index++)
@@ -163,18 +221,49 @@ float calc_sum_of_x_squared(float coord_list[][2], int rows)
 }
 void calc_gradient(int rows, float sum_of_x, float sum_of_y, float sum_of_pdt, float sum_of_x_squared)
 {
+    /* Purpose: 
+        Calculates gradient.
+       Inputs: 
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+        float sum_of_x - The sum of all x values.
+        float sum_of_y - The sum of all y values.
+        float sum_of_pdt - The sum of the product of each x value and its corresponding y value.
+        float sum_of_x_squared - The sum of each x value squared.
+       Returned value:
+        void. The value calculated is printed directly.
+    */
     float gradient;
     gradient = ((rows * sum_of_pdt) - (sum_of_x * sum_of_y)) / ((rows * sum_of_x_squared) - (sum_of_x * sum_of_x));
     printf("\nm = %f", gradient);
 }
 void calc_offset(int rows, float sum_of_x, float sum_of_y, float sum_of_pdt, float sum_of_x_squared)
 {
+    /* Purpose: 
+        Calculates offset.
+       Inputs: 
+        int rows - The number of coordinate pairs. This was previously extracted using (int)validate_input('r')
+        float sum_of_x - The sum of all x values.
+        float sum_of_y - The sum of all y values.
+        float sum_of_pdt - The sum of the product of each x value and its corresponding y value.
+        float sum_of_x_squared - The sum of each x value squared.
+       Returned value:
+        void. The value calculated is printed directly.
+    */
     float offset;
     offset = ((sum_of_x_squared * sum_of_y) - (sum_of_x * sum_of_pdt)) / ((rows * sum_of_x_squared) - (sum_of_x * sum_of_x));
     printf("\nc = %f", offset);
 }
 int retry_request()
 {
+    /* Purpose: 
+        Places a retry request. Contains input validation to ensure either y/n.
+       Inputs: 
+        None
+       Returned value:
+        int- If = 1, the whole process restarts. 
+             Else If = 0, Goodbye is printed.
+             A default return value of -1. 
+    */
     int valid = 0;
     do
     {
